65db06b208822cff604fcbf049e36bdf394fc430
ArneDev1 <39313252+ArneDev1@users.noreply.github.com>
2019-05-13 16:47:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = "!"
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '{prefix}ping') {
     	message.channel.send('PONG!');
   	}
 });
